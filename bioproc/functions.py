e42235edd36f24c97f25c6f3fb95a4bb01d318e6
praveenprabhakarkr <praveenprabhakar02@gmail.com>
2020-03-28 00:13:48 -0400

    Functions file updated - 03/28/2020

--- bioproc/functions.py
+++ bioproc/functions.py
@@ -1,14 +1,14 @@
 
 import numpy as np
-import scipy
-from scipy.fftpack import fft
+import scipy as sp
 import matplotlib.pyplot as plt
 %matplotlib inline 
 #for Jupyter notebook
 
     
-def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0):
+def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, plot='yes', tarr='no'):
     """ 
+    
     This function is used to generate sine waves without any noise.
     
     Input parameters
@@ -20,7 +20,15 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0):
     time: int or float
         time period of the sine wave; set to 10 secs by default
     fs: int or float
-        sampling frequency; set to 1000 Hz by default
+        sampling frequency; set to 100 Hz by default
+    phi: int or float
+        phase (angle); set to 0 degrees by default
+    offset: int or float
+        bias; set to 1 by default
+    plot: string - yes or no
+        Plot the sine wave or not; default set to yes
+    tarr: string - yes or no
+        return the time array or not; default set to no
         
     Output
     ------
@@ -29,7 +37,7 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0):
     t: int or float
        time of sine wave in secs
     sine: sine wave
-        amplitude of sine wave
+        amplitude of the sine wave
     
     """ 
     
@@ -37,32 +45,75 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0):
     t = np.arange(0,time, 1/fs)
     sine = offset + np.sin((2*pi*freq*t) + phi)
     
-    return t,sine
+    if plot == 'yes':
+        plt.plot(t,sine)
+        plt.title("Sine wave")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude (m)")
     
+    if tarr == 'yes':
+        return t,sine
+    else:
+        return sine
     
-def sinenoise(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0):
+def sinenoise(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, noise = 1, plot='yes', tarr='no'):
     """
-    This function 
+    
+    This function is used to generate a sine wave with random noise.
     
     Input parameters
     ----------------
-    
-    
+    amp: int or float
+        amplitude of the sine wave; set to 1 by default
+    freq: int or float 
+        frequency of the sine wave; set to 1 Hz by default
+    time: int or float
+        time period of the sine wave; set to 10 secs by default
+    fs: int or float
+        sampling frequency; set to 1000 Hz by default
+    phi: int or float
+        phase (angle); set to 0 degrees by default
+    offset: int or float
+        bias; set to 1 by default
+    noise: int or float
+        control the level of noise; increasing the value reduces the noise
+        and vice versa
+    plot: string - yes or no
+        Plot the sine wave or not; default set to yes
+    tarr: string - yes or no
+        return the time array or not; default set to no 
+        
     Output
     ------
+    Output will be in the format --> t,sine
     
+    t: int or float
+       time of sine wave in secs
+    sine: sine wave
+        amplitude of the sine wave
+        
     """
     
     pi = 3.14
     t = np.arange(0,time, 1/fs)
-    sine = offset + np.sin((2*pi*freq*t) + phi) + (np.random.randn(len(t))/10)
+    sine = offset + np.sin((2*pi*freq*t) + phi) + (np.random.randn(len(t))/noise)
     
-    return sine
+    if plot == 'yes':
+        plt.plot(t,sine)
+        plt.title("Sine wave with noise")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude (m)")
+        
+    if tarr == 'yes':
+        return t,sine
+    else:
+        return sine
 
 
-def fft(sin, n=None, axis= -1, o=False):
+def fft(sinwave, fs = 1000, n=None, axis= -1, plot='yes'):
     """
-    This function 
+    
+    This function is used to calculate the discrete fourier transform of the sine wave.
     
     Input parameters
     ----------------
@@ -71,14 +122,30 @@ def fft(sin, n=None, axis= -1, o=False):
     Output
     ------
     
+    
     """
+    n = axis
+    fourier = np.fft.fft(sinwave, axis = n)
+    N = sinwave.size
+    amp = np.linspace(0, fs, N)
     
+    if plot == 'yes':
+        
+        plt.ylabel("Amplitude")
+        plt.xlabel("Frequency (Hz)")
+        plt.bar(amp[:N // 2], np.abs(fourier)[:N // 2], width=1.5)
+        plt.show
+
+        return fourier
     
-    return fourier
+    else:
+        
+        return fourier
 
 
 def padding(wave):
     """
+    
     This function 
     
     Input parameters
@@ -227,7 +294,7 @@ def ccorr(array):
 
 
 def acorr(array):
-"""
+    """
     This function 
     
     Input parameters
@@ -242,3 +309,69 @@ def acorr(array):
 
     return acorrarray
 
+
+def psd(array):
+    """
+    This function 
+    
+    Input parameters
+    ----------------
+    
+    
+    Output
+    ------
+    
+    """
+    
+    
+    return narray
+
+
+def rectify(array):
+    """
+    This function 
+    
+    Input parameters
+    ----------------
+    
+    
+    Output
+    ------
+    
+    """
+    
+    
+    return narray
+
+def integrate(array):
+    """
+    This function 
+    
+    Input parameters
+    ----------------
+    
+    
+    Output
+    ------
+    
+    """
+    
+    
+    return narray
+
+
+def rms(array):
+    """
+    This function 
+    
+    Input parameters
+    ----------------
+    
+    
+    Output
+    ------
+    
+    """
+    
+    
+    return narray
