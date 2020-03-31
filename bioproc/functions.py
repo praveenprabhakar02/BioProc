3016e56e6ea74be6381126638130104931a3ffb6
praveenprabhakarkr <praveenprabhakar02@gmail.com>
2020-03-31 03:19:00 -0400

    Added FFT and movacg functions. 3/31/2020

--- bioproc/functions.py
+++ bioproc/functions.py
@@ -2,8 +2,6 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-%matplotlib inline 
-#for Jupyter notebook
 
     
 def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, plot='yes', tarr='no'):
@@ -13,21 +11,21 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, plot='yes', tarr
     
     Input parameters
     ----------------
-    amp: int or float
+    amp: int or float, optional
         amplitude of the sine wave; set to 1 by default
-    freq: int or float 
+    freq: int or float, optional 
         frequency of the sine wave; set to 1 Hz by default
-    time: int or float
+    time: int or float, optional
         time period of the sine wave; set to 10 secs by default
-    fs: int or float
+    fs: int or float, optional
         sampling frequency; set to 100 Hz by default
-    phi: int or float
+    phi: int or float, optional
         phase (angle); set to 0 degrees by default
-    offset: int or float
+    offset: int or float, optional
         bias; set to 1 by default
-    plot: string - yes or no
-        Plot the sine wave or not; default set to yes
-    tarr: string - yes or no
+    plot: string - yes or no, optional
+        plot the sine wave or not; default set to yes
+    tarr: string - yes or no, optional
         return the time array or not; default set to no
         
     Output
@@ -35,7 +33,7 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, plot='yes', tarr
     Output will be in the format --> t,sine
     
     t: int or float
-       time of sine wave in secs
+        time of sine wave in secs
     sine: sine wave
         amplitude of the sine wave
     
@@ -45,15 +43,15 @@ def sinewave(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, plot='yes', tarr
     t = np.arange(0,time, 1/fs)
     sine = offset + np.sin((2*pi*freq*t) + phi)
     
-    if plot == 'yes':
+    if plot == 'yes' or plot == 'Yes':
         plt.plot(t,sine)
         plt.title("Sine wave")
         plt.xlabel("Time (s)")
         plt.ylabel("Amplitude (m)")
     
-    if tarr == 'yes':
+    if tarr == 'yes' or tarr == 'Yes':
         return t,sine
-    else:
+    elif tarr == 'no' or tarr == 'No':
         return sine
     
 def sinenoise(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, noise = 1, plot='yes', tarr='no'):
@@ -63,24 +61,23 @@ def sinenoise(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, noise = 1, plot
     
     Input parameters
     ----------------
-    amp: int or float
+    amp: int or float, optional
         amplitude of the sine wave; set to 1 by default
-    freq: int or float 
+    freq: int or float, optional
         frequency of the sine wave; set to 1 Hz by default
-    time: int or float
+    time: int or float, optional
         time period of the sine wave; set to 10 secs by default
-    fs: int or float
+    fs: int or float, optional
         sampling frequency; set to 1000 Hz by default
-    phi: int or float
+    phi: int or float, optional
         phase (angle); set to 0 degrees by default
-    offset: int or float
+    offset: int or float, optional
         bias; set to 1 by default
-    noise: int or float
-        control the level of noise; increasing the value reduces the noise
-        and vice versa
-    plot: string - yes or no
-        Plot the sine wave or not; default set to yes
-    tarr: string - yes or no
+    noise: int or float, optional
+        control the level of noise; increasing the value reduces the noise and vice versa
+    plot: string - yes or no, optional
+        plot the sine wave or not; default set to yes
+    tarr: string - yes or no, optional
         return the time array or not; default set to no 
         
     Output
@@ -98,15 +95,15 @@ def sinenoise(amp=1, freq=1, time = 10, fs=100, phi=0, offset=0, noise = 1, plot
     t = np.arange(0,time, 1/fs)
     sine = offset + np.sin((2*pi*freq*t) + phi) + (np.random.randn(len(t))/noise)
     
-    if plot == 'yes':
+    if plot == 'yes' or plot == 'Yes':
         plt.plot(t,sine)
         plt.title("Sine wave with noise")
         plt.xlabel("Time (s)")
         plt.ylabel("Amplitude (m)")
         
-    if tarr == 'yes':
+    if tarr == 'yes' or tarr == 'Yes':
         return t,sine
-    else:
+    elif tarr == 'no' or tarr == 'No':
         return sine
 
 
@@ -117,11 +114,24 @@ def fft(sinwave, fs = 1000, n=None, axis= -1, plot='yes'):
     
     Input parameters
     ----------------
+    sinwave: ndarray
+        the input signal whose DFT is to be determined
+    fs: int or float, optional
+        sampling frequency; default set to 1000 Hz
+    n: int, optional
+        length of the transformed axis of the output; default set to None
+    axis: int, optional
+        axis over which to compute FFT; default set to -1
+    plot: string - yes or no, optional
+        plot the fourier or not; default set to yes
     
     
     Output
     ------
+    Output will be in the format --> fourier
     
+    fourier: ndarray
+        FFT of the input signal
     
     """
     n = axis
@@ -129,16 +139,16 @@ def fft(sinwave, fs = 1000, n=None, axis= -1, plot='yes'):
     N = sinwave.size
     amp = np.linspace(0, fs, N)
     
-    if plot == 'yes':
+    if plot == 'yes' or plot == 'Yes':
         
         plt.ylabel("Amplitude")
         plt.xlabel("Frequency (Hz)")
-        plt.bar(amp[:N // 2], np.abs(fourier)[:N // 2], width=1.5)
+        plt.bar(amp[:N // 2], np.abs(fourier)[:N // 2]*1/N, width=1.5)
         plt.show
 
         return fourier
     
-    else:
+    elif plot == 'no' or plot == 'No':
         
         return fourier
 
@@ -211,21 +221,38 @@ def fir(wave):
     
     return nwave
 
-def movavg(wave):
+def movavg(signal, n=3):
     """
-    This function 
+    This function returns the moving average of a signal.
     
     Input parameters
     ----------------
-    
+    signal: ndarray
+        the input signal whose moving average is to be determined
+    n: int, optional
+        the number of points used for moving average
     
     Output
     ------
+    Output will be in the format --> moveaverage
+    
+    movaverage: ndarray
+        the moving average of the signal    
     
     """
+    count = 0
+    temp = signal.size
+    movaverage = []
+
+    while((count + n) <= temp):
+
+        temp1 = np.sum(signal[count:(count+n)]) / n
+        movaverage.append(temp1)
+        count += 1
+
+    movaverage = np.array(movaverage)
     
-    
-    return nwave
+    return movaverage
 
 
 def polyfit(wave):
