20eecb7fa5cc6158be51d263374f85229f9f6b10
praveenprabhakarkr <praveenprabhakar02@gmail.com>
2020-04-24 02:45:25 -0400

    Updated docstring. Find_onset testing.

--- bioproc/test/test_emg.py
+++ bioproc/test/test_emg.py
@@ -11,6 +11,10 @@ import numpy as np
 from .. import emg as em
 
 def test_emgsig():
+    """
+    Test for emgsig function in the emg module.
+    """
+
     with pytest.raises(NameError):
         em.emgsig(seed=s)
     with pytest.raises(TypeError):
@@ -40,32 +44,75 @@ def test_emgsig():
 
 
 def test_find_onset():
+    """
+    Test for find_onset function in the emg module.
+    """
+
+    with pytest.raises(ValueError):
+        em.find_onset(signal=1)
+    with pytest.raises(TypeError):
+        em.find_onset(signal='sa')
+    with pytest.raises(NameError):
+        em.find_onset(signal=sa)
+    with pytest.raises(TypeError):
+        em.find_onset(signal=1+3j)
+    with pytest.raises(ValueError):
+        em.find_onset(signal=[1, 1+3j])
     signal = em.emgsig()
     with pytest.raises(ValueError):
-        em.emgsig(signal, plot='sa')
-    with pytest.raises(TypeError):
-        em.emgsig(signal, plot=123)
+        em.find_onset(signal, fs='s')
     with pytest.raises(NameError):
-        em.emgsig(signal, plot=s)
+        em.find_onset(signal, fs=s)
+    with pytest.raises(TypeError):
+        em.find_onset(signal, fs=[1, 2])
+    with pytest.raises(TypeError):
+        em.find_onset(signal, fs=1+3j)
+    with pytest.raises(TypeError):
+        em.find_onset(signal, fs=[1+3j])
     with pytest.raises(ValueError):
-        em.emgsig(signal, tarr='sa')
+        em.find_onset(signal, plot='sa')
     with pytest.raises(TypeError):
-        em.emgsig(signal, tarr=123)
+        em.find_onset(signal, plot=123)
     with pytest.raises(NameError):
-        em.emgsig(signal, tarr=s)
+        em.find_onset(signal, plot=s)
     with pytest.raises(TypeError):
-        em.emgsig(signal, filt=1+3j)
+        em.find_onset(signal, plot=1+3j)
     with pytest.raises(TypeError):
-        em.emgsig(signal, plot=1+3j)
+        em.find_onset(signal, plot=[1+3j])
+    with pytest.raises(ValueError):
+        em.find_onset(signal, filt='sa')
+    with pytest.raises(TypeError):
+        em.find_onset(signal, filt=123)
+    with pytest.raises(NameError):
+        em.find_onset(signal, filt=s)
+    with pytest.raises(TypeError):
+        em.find_onset(signal, filt=1+3j)
+    with pytest.raises(TypeError):
+        em.find_onset(signal, filt=[1+3j])
 
 
 def test_norm_emg():
+    """
+    Test for norm_emg function in the emg module.
+    """
+
+    
     return None
 
 
 def test_padding():
+    """
+    Test for padding function in the emg module.
+    """
+
+    
     return None
 
 
 def test_emg_process():
+    """
+    Test for emg_process function in the emg module.
+    """
+
+    
     return None
